4e4c40177b0eab4263d1512c0ed65a95bcdedcbe
zhouxiang <524347085@qq.com>
2020-09-05 09:21:21 +0800

    update code

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -2,20 +2,20 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.less";
 
-function App() {
+const App = () => {
   return (
-    <div className="app">
-      <div className="loginPage">
-        <img src={logo} className="logo" alt="logo" />
-        <div className="loginForm">
-          <h3>Login</h3>
-          <input type="text" placeholder="Please enter your username" />
-          <input type="password" placeholder="Please enter your password" />
-          <button>登录</button>
+      <div className="app">
+        <div className="loginPage">
+          <img src={logo} className="logo" alt="logo" />
+          <div className="loginForm">
+            <h3>Login Page</h3>
+            <input type="text" placeholder="Please enter your username" />
+            <input type="password" placeholder="Please enter your password" />
+            <button>Login</button>
+          </div>
         </div>
       </div>
-    </div>
   );
-}
+};
 
 export default App;
