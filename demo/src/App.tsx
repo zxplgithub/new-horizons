b7919c500aeb60ea9cacbe11b0c5650585682082
zhouxiang <524347085@qq.com>
2020-09-05 08:56:30 +0800

    change login page .less

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.less";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="loginPage">
+        <img src={logo} className="logo" alt="logo" />
+        <div className="loginForm">
+          <h3>Login</h3>
+          <input type="text" placeholder="Please enter your username" />
+          <input type="password" placeholder="Please enter your password" />
+          <button>登录</button>
+        </div>
+      </div>
     </div>
   );
 }
